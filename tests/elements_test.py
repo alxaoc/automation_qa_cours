4e6fa10c90001691154043c09cd2d9d25bd36714
MoToR <alex.bublik.82@gmail.com>
2023-02-06 01:24:26 +0200

    Lesson 02, fixed go_to_element in base_page.py update elements test added test checkboxes added location

--- tests/elements_test.py
+++ tests/elements_test.py
@@ -1,6 +1,6 @@
 import time
+from pages.elements_page import TextBoxPage, CheckBoxPage
 
-from pages.elements_page import TextBoxPage
 
 class TestElements:
     class TestTextBox:
@@ -8,15 +8,19 @@ class TestElements:
         def test_text_box(self, driver):
             text_box_page = TextBoxPage(driver, 'https://demoqa.com/text-box')
             text_box_page.open()
-            # code small
-            #input_date = text_box_page.fill_all_fields()
-            #output_date = text_box_page.check_filled_form()
-            #assert input_date == output_date
-            # code full
             full_name, email, current_address, permanent_address = text_box_page.fill_all_fields()
             output_name, output_email, output_cur_addr, output_per_addr = text_box_page.check_filled_form()
             assert full_name == output_name, "The full name does not match!"
             assert email == output_email, "The email does not match!"
             assert current_address == output_cur_addr, "The current address does not match!"
             assert permanent_address == output_per_addr, "The permanent address not match!"
-            time.sleep(5)
+
+    class TestCheckBox:
+        def test_check_box(self, driver):
+            check_box_page = CheckBoxPage(driver, 'https://demoqa.com/checkbox')
+            check_box_page.open()
+            check_box_page.open_full_list()
+            check_box_page.click_random_checkbox()
+            input_checkbox = check_box_page.get_checked_checkboxes()
+            output_result = check_box_page.get_output_result()
+            assert input_checkbox == output_result, 'Checkboxes have not been selected!'
